c7f82a28818720e760bdf2453988694f0bffb7cf
Parker Mei <parkermeicollege@gmail.com>
2024-05-26 20:29:44 -0700

    2nd Push

--- ImaingScience.py
+++ ImaingScience.py
@@ -1,1 +1,9 @@
-print("Hello")
+import cv2 as cv
+import sys
+
+img = cv.imread("/Users/parkermei/ImagingScience/baboon.png")
+#cv.imshow("Display",img)
+#k = cv.waitKey(0)
+
+px = img[100,100]
+print(px)
